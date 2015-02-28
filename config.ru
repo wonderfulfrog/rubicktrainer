9be45a3ad189a2d3f356bfb324fefeed1e9007f3
Devin Lumley <keiroshin@gmail.com>
2015-02-28 00:29:45 -0800

    Updating config for Sinatra

--- config.ru
+++ config.ru
@@ -1,2 +1,3 @@
 require './web'
-run Sinatra::Application
+
+run Web
--- web.rb
+++ web.rb
@@ -1,33 +1,22 @@
-# a super-trivial Sinatra-based webserver
-# for static content
 require 'sinatra'
+require 'rack/tracker'
 
 class Web < Sinatra::Base
 
-# set all the settings!
-configure do
-  # this is arguably not necessary... 'public'
-  # folder is the static content location by default
-  set :public_folder, 'public'
+  configure do
 
-  # optionally configure Cache-Control headers on responses
-  # set :static_cache_control, [:public, :max_age => 300]
+    set :public_folder, 'public'
 
-  # if using mime types not known to Sinatra, uncomment and
-  # configure here (by file extension)
-  # mime_type :foo, 'text/foo'
-end
+  end
 
-# serve the files!
+  configure :production do
+    use Rack::Tracker do
+      handler :google_analytics, { tracker: 'U-XXXXX-Y' }
+    end
+  end
 
-# route to starting page (index.html)
-get "/" do
-  redirect '/index.html'
-end
-
-# route to custom error page (404.html)
-not_found do
-  redirect '/404.html'
-end
+  get "/" do
+    redirect '/index.html'
+  end
 
 end
