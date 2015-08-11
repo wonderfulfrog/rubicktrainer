451b3f99758ae3bf16f727ad6d2b0351115ec554
Devin Lumley <keiroshin@gmail.com>
2015-08-10 17:19:38 -0700

    Adding EJS, adding GA tracking code for production environment

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var app 	= express();
 var port    = 2525;
 
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {  
+  res.render('index');
+});
 
 console.log("Server listening on port http://localhost:%s", port);
 
